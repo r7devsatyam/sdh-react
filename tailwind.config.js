5b94f5be1141c8c766105f14ac9c1efa28afb9e6
Satyam Singh <r7devsatyam@gmail.com>
2021-09-14 12:30:42 +0000

    Review for CSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: [".src/Styles/index.css"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
